d624773ac7301f7083887baaae919b42ea388fd6
ethan <ethanmannette@gmail.com>
2015-07-30 15:52:23 -0600

    Removed styling from first two boxes in second row. Set width of all non secret box divs to 2px

--- boxes.js
+++ boxes.js
@@ -3,12 +3,34 @@ $(document).ready(function() {
   console.log("sanity check");
   // alert ("Ready for DOM manipulation");
 
+  // sets background of secret box div to white
   $('#secretBox').css({"background-color": "white"});
 
-  $('#row1').children().removeClass('boxType1 boxType2').addClass('boxType3');
+  // sets class of boxType1 and boxType2 in row 1 to boxType3
+  $('#row1').children()
+  .removeClass('boxType1 boxType2')
+  .addClass('boxType3');
 
-  $('#row4').children().last().hide();
+  // hides last div in row 4
+  $('#row4').children()
+  .last()
+  .hide();
 
-  $('#container').children().children('.boxType1').css('background-color', 'white');
+  // sets background of all red boxes to white
+  $('#container').children()
+  .children('.boxType1')
+  .css('background-color', 'white');
 
+  // removes styling on first two boxes by removing classes
+  var firstBox = $('#row2').children().first();
+  var secondBox = firstBox.next();
+  firstBox.removeClass('boxType1 boxType2 boxType3');
+  secondBox.removeClass('boxType1 boxType2 boxType3');
+
+  // selects all box divs with the exception of secret box
+  $('#container')
+  .children()
+  .children('.box')
+  .not('#secretBox')
+  .css('width', '2px');
 });
