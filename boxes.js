227fd5775f9f837a019c7d8f030b37ecae231d89
ethan <ethanmannette@gmail.com>
2015-07-30 13:31:52 -0600

    Changed first row class to boxType3. Changed red boxes to white. Last box in last row is hidden.

--- boxes.js
+++ boxes.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+$(document).ready(function() {
+
+  console.log("sanity check");
+  // alert ("Ready for DOM manipulation");
+
+  $('#secretBox').css({"background-color": "white"});
+
+  $('#row1').children().removeClass('boxType1 boxType2').addClass('boxType3');
+
+  $('#row4').children().last().hide();
+
+  $('#container').children().children('.boxType1').css('background-color', 'white');
+
+});
